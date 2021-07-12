e6545cb9d8a6ad5e97b64f715a24719a2d58e858
jennaleonard <jleonardweb@gmail.com>
2021-07-12 08:41:01 -0700

    Updated components, styling, and added menu items.

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, ScrollView, SafeAreaView } from "react-native";
 import MenuItem from "./components/MenuItem";
+import sushi from "./assets/sushi.jpeg";
+import burger from "./assets/burger.jpeg";
+
+const items = {
+  name: ['sushi', 'burger'],
+  price: [200, 15],
+  imageSource: [sushi, burger],
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>Snap Eating Account</Text>
-      {/* TODO (part 1): update these props with your favorite food item */}
-      <MenuItem name={null} price={null} imageSource={null} />
-      {/* TODO (part 2): add another MenuItem! */}
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <Text style={styles.title}>Snap Eating Account</Text>
+        <MenuItem name={items.name[0]} price={items.price[0]} imageSource={items.imageSource[0]} />
+        <MenuItem name={items.name[1]} price={items.price[1]} imageSource={items.imageSource[1]} />
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
+    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    padding: "8%",
+  }
 });
--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,67 +1,76 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, Image, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, Image, TextInput, SafeAreaView, ScrollView, StatusBar} from "react-native";
 
 function MenuItem(props) {
   // Keep track of quantity
   const [quantity, setQuantity] = useState(0);
   // TODO (part 3): add state for special instructions text
-
+  const [instructions, setInstructions] = useState('');
   // Return JSX to render
   return (
-    <View style={styles.container}>
-      <View style={styles.photoContainer}>
-        <Image
-          source={require("../assets/placeholder-image.png")}
-          style={styles.photo}
-        />
-      </View>
-      <Text style={{ fontWeight: "bold" }}>{"FOOD NAME"}</Text>
-      <Text>Price: ${1000}</Text>
-      <Text>Quantity: {quantity}</Text>
-      <View style={styles.buttonsContainer}>
-        <Button
-          title="-"
-          onPress={() => {
-            console.log("minus pressed");
-            // TODO (part 2): decrease quantity by 1
-            // watch out for negative quantity
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.photoContainer}>
+          <Image
+            source={props.imageSource}
+            style={styles.photo}
+          />
+        </View>
+        <Text style={{ fontWeight: "bold" }}>{props.name}</Text>
+        <Text>Price: ${props.price}</Text>
+        <Text>Quantity: {quantity}</Text>
+        <View style={styles.buttonsContainer}>
+          <Button
+            title="-"
+            onPress={() => {
+              console.log("minus pressed");
+              if (quantity > 0) {
+                setQuantity(quantity - 1);
+              };
+            }}
+          />
+          <Button
+            title="+"
+            onPress={() => {
+              console.log("plus pressed");
+              setQuantity(quantity + 1);
+            }}
+          />
+        </View>
+        <Text>Special Instructions: {instructions}</Text>
+        <TextInput
+          placeholder="Type instructions here"
+          onSubmitEditing={({ nativeEvent, currentTarget }) => {
+            console.log(nativeEvent.text);
+            // TODO (part 3): Update special instructions text
+            setInstructions(nativeEvent.text);
+            currentTarget.clear();
           }}
         />
-        <Button
-          title="+"
-          onPress={() => {
-            console.log("plus pressed");
-            // TODO (part 2): increase quantity by 1
-          }}
-        />
-      </View>
-      <Text>Special Instructions: {null}</Text>
-      <TextInput
-        placeholder="Type instructions here"
-        onSubmitEditing={({ nativeEvent }) => {
-          console.log(nativeEvent.text);
-          // TODO (part 3): Update special instructions text
-          nativeEvent.target.clear();
-        }}
-      />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
-    rowGap: "10px",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "space-between",
     borderStyle: "solid",
-    borderWidth: "2px",
+    borderWidth: 2,
     width: "90%",
     padding: 30,
+    paddingTop: StatusBar.currentHeight,
     margin: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // marginHorizontal: 20,
   },
   photoContainer: {
     padding: 5,
@@ -69,6 +78,7 @@ const styles = StyleSheet.create({
   photo: {
     resizeMode: "contain",
     width: "75%",
+    height: 0,
     padding: 50,
   },
   buttonsContainer: {
